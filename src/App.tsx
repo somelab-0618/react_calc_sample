dd766c447e860cbfc04fd080028d93de31c27cdc
RyosukeSomeya <rx.0.0618@gmail.com>
2022-02-18 00:51:47 +0900

    react calc sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { ReactElement, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, ReactElement, useState } from 'react';
 import './App.css';
 
 function App(): ReactElement {
-  // const [msg, setMsg] = useState('This is sample message');
-  const [counter, setCounter] = useState(0);
-  const doAction = () => {
-    setCounter(counter + 1);
+  const [val, setVal] = useState(0);
+  const [data, setData] = useState<number[]>([]);
+
+  const doChange = (event: ChangeEvent): void => {
+    const ob = event.target as HTMLInputElement;
+    const re = Number(ob.value);
+    setVal(re);
   };
+
+  const doAction = (): void => {
+    const arr: number[] = [];
+    for (let item of data) {
+      arr.push(item);
+    }
+    arr.push(val);
+    setData(arr);
+    setVal(0);
+  };
+
+  const doType = (event: KeyboardEvent<HTMLInputElement>): void => {
+    if (event.code == 'Enter') {
+      doAction();
+    }
+  };
+
+  let total = 0;
   return (
     <div>
       <h1 className='bg-primary text-white p-2'>React sample</h1>
@@ -14,13 +35,36 @@ function App(): ReactElement {
         <h2 className='my-3'>click button!</h2>
         <div className='alert alert-primary'>
           <div className='row px-2'>
-            <h3 id='msg' className='col'>
-              {counter} click
-            </h3>
+            <input
+              type='number'
+              className='col'
+              onChange={doChange}
+              onKeyPress={doType}
+              value={val}
+            />
             <button onClick={doAction} className='btn btn-primary col-2'>
               click
             </button>
           </div>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>value</th>
+                <th>total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((v, k) => {
+                total += v;
+                return (
+                  <tr key={k}>
+                    <td>{v}</td>
+                    <td>{total}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
