0227524cf6db5d69fae1e2b912a798b7ee2123ad
RyosukeSomeya <rx.0.0618@gmail.com>
2022-02-18 00:22:15 +0900

    counter sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ReactElement, useState } from 'react';
 import './App.css';
 
-function App() {
+function App(): ReactElement {
+  // const [msg, setMsg] = useState('This is sample message');
+  const [counter, setCounter] = useState(0);
+  const doAction = () => {
+    setCounter(counter + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='bg-primary text-white p-2'>React sample</h1>
+      <div className='container'>
+        <h2 className='my-3'>click button!</h2>
+        <div className='alert alert-primary'>
+          <div className='row px-2'>
+            <h3 id='msg' className='col'>
+              {counter} click
+            </h3>
+            <button onClick={doAction} className='btn btn-primary col-2'>
+              click
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
